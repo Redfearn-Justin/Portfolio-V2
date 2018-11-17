4b027ab988c1215d8dcbab932478151e1ad4c4e8
Napoleon <redfearn_justin@yahoo.com>
2018-11-16 22:51:29 -0800

    Updated portfolio/outline for fb encryption

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,13 +2,22 @@ $(document).ready(function() {
 
     // Initialize Firebase
     //=======================================================
+    // var config = {
+    //     apiKey: process.env.FIREBASE_API_KEY,
+    //     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    //     databaseURL: process.env.FIREBASE_DATABASE_URL,
+    //     projectId: process.env.FIREBASE_PROJECT_ID,
+    //     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    //     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+    // };
+
     var config = {
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        databaseURL: process.env.FIREBASE_DATABASE_URL,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+        apiKey: "AIzaSyCUc2p24x8ASUskcbEeIbhNyeh7jxriZoY",
+        authDomain: "portfolio-intel.firebaseapp.com",
+        databaseURL: "https://portfolio-intel.firebaseio.com",
+        projectId: "portfolio-intel",
+        storageBucket: "portfolio-intel.appspot.com",
+        messagingSenderId: "725334943993"
     };
 
     firebase.initializeApp(config);
@@ -77,7 +86,7 @@ $(document).ready(function() {
     
         //upload object to firebase
     
-        database.ref().push(totalUserInput);
+        DB.ref().push(totalUserInput);
     
     
         //function to clear fields after user input
