599bb9d3cca2bbabfc068b446013d28967a518e1
Napoleon <redfearn_justin@yahoo.com>
2018-11-16 20:45:59 -0800

    Encrypted FB

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
     // Initialize Firebase
     //=======================================================
     var config = {
-        apiKey: "AIzaSyCUc2p24x8ASUskcbEeIbhNyeh7jxriZoY",
-        authDomain: "portfolio-intel.firebaseapp.com",
-        databaseURL: "https://portfolio-intel.firebaseio.com",
-        projectId: "portfolio-intel",
-        storageBucket: "portfolio-intel.appspot.com",
-        messagingSenderId: "725334943993"
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        databaseURL: process.env.FIREBASE_DATABASE_URL,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
     };
 
     firebase.initializeApp(config);
